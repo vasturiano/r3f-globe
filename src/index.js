ebc51fb3c6217f31d15a5c3bd92a22b3600f87d2
vasco <vastur@gmail.com>
2024-12-09 00:59:18 +0000

    Improve object detection logic on hover

--- src/index.js
+++ src/index.js
@@ -8,21 +8,30 @@ const GlobeComp = fromThree(ThreeGlobe, {
   methodNames: ['getGlobeRadius', 'getCoords', 'toGeoCoords', 'pauseAnimation', 'resumeAnimation']
 });
 
-const getGlobeEventObj = ({ intersections }) => intersections.find(d =>
-  d?.object?.hasOwnProperty('__globeObjType') && d.object.__globeObjType !== 'atmosphere')?.object;
+const getGlobeEventObj = ({ intersections }) => {
+  const lookupGlobeObj = o => // recurse up object chain until finding the globe object
+    !o ? null : o.hasOwnProperty('__globeObjType') ? o : lookupGlobeObj(o.parent);
+
+  return lookupGlobeObj(intersections.find(d => {
+    const gObj = lookupGlobeObj(d.object);
+    return gObj && gObj.__globeObjType !== 'atmosphere';
+  })?.object);
+};
+
+const getObjData = obj => obj?.__globeObjType === 'polygon' ? obj?.__data?.data : obj?.__data;
 
 const Globe = forwardRef(({ onHover, onClick, ...ptProps }, ref) => {
   const [curHoverObj, setCurHoverObj] = useState('init');
 
   useEffect(() => {
-    onHover && curHoverObj !== 'init' && onHover(curHoverObj?.__globeObjType, curHoverObj?.__data);
+    onHover && curHoverObj !== 'init' && onHover(curHoverObj?.__globeObjType, getObjData(curHoverObj));
   }, [curHoverObj, onHover]);
 
   const onHoverInt = useCallback((e) => setCurHoverObj(getGlobeEventObj(e)), [setCurHoverObj]);
   const onClickInt = useCallback((e) => {
     const obj = getGlobeEventObj(e);
     if(obj && onClick) {
-      onClick(obj?.__globeObjType, obj?.__data, e);
+      onClick(obj?.__globeObjType, getObjData(obj), e);
       e.stopPropagation();
     }
   }, [onClick]);
